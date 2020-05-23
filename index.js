abf70f59725674b0a8bc09f3a1e98607ea2fedb1
Maximiliano Pomar <mpomar@simtlix.com>
2020-05-22 21:51:19 -0300

    Change the base project to AwesomeProject

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-// Index.ios.js - place code in here for IOS!!!!
+/**
+ * @format
+ */
 
-// Import a library to help create a component
-import React from 'react';
-import { AppRegistry, View } from 'react-native';
-import Header from './src/components/header';
-import AlbumList from './src/components/AlbumList';
-import PhotoList from './src/components/PhotoList';
-import { Router, Scene } from 'react-native-router-flux';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-// Create a component
-const App = () => (
-      <Router>
-        <Scene key="root">
-          <Scene key="albumList" component={AlbumList} title="Albums" initial={true} />
-          <Scene key="photoList" component={PhotoList} title="Photos" />
-        </Scene>
-      </Router>
-);
-
-// Render it to the device
-AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent(appName, () => App);
